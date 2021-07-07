f2c0d9adb0a74b27413b8c85e9d1d068684684eb
azamifarda <arash.azamifard@nersc.no>
2021-07-07 11:17:24 +0200

    #1 forgotten files

---
+++ regridder/formats/opening.py
@@ -0,0 +1,15 @@
+from exceptions import BadAreaDefinition
+from .geodataset import GeoDataset
+from .mooring import Moorings
+
+def open_netcdf(file_address):
+    classes = [GeoDataset, Moorings]
+    for class_ in classes:
+        try:
+            obj = class_(file_address)
+        except BadAreaDefinition:
+            continue # skip to the next class in the list
+        return obj # return object when try was succesful
+
+    # raise error when none of classes suited
+    raise ValueError("Can not find proper geodataset-based class for this file: " + file_address)
