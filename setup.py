827bc4c4c260632945a09267fee7fb463cc579c6
Anton Korosov <korosov@gmail.com>
2022-01-11 11:38:54 +0100

    actions: add build, publish

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -5,18 +6,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geodataset",
-    version="0.1.0",
+    version=os.getenv('GEODATASET_RELEASE', '0.1.0').lstrip('v'),
     author=["Anton Korosov", "Timothy Williams"],
     author_email="Anton.Korosov@nersc.no",
     description="Extension of netDCF4.Dataset for geospatial data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nansencenter/geodataset",
+    test_suite='geodataset.tests',
+    license='GPLv3',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: POSIX :: Linux",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Utilities',
     ],
     install_requires=[
         "cartopy",
