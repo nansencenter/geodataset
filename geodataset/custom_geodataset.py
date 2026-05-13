2b30f0971cfdcf2be78b033552983b18f7d45324
tdcwilliams <tdcwilliams@gmail.com>
2026-05-13 13:48:14 +0200

    bug in AWISMOSCS2S3Thickness.datetime_bounds

--- geodataset/custom_geodataset.py
+++ geodataset/custom_geodataset.py
@@ -260,5 +260,5 @@ class AWISMOSCS2S3Thickness(CustomDatasetRead):
             dto2: datetime.datetime
                 end of observation interval
         """
-        t_bnds = self["time_bnds"][:].values.flatten()
+        t_bnds = self["time_bnds"][:].data.flatten()
         return [self.convert_time_data(t_bnds)]
